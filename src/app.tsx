0f07bb1b46d900648ec42a8a6f2b62405bdc21c8
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-19 23:35:48 +0530

    Updated code files: app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,44 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/ThemeProvider"; // Added import
+
+import ArtistDetailPage from "./pages/ArtistDetailPage";
+import HomePage from "./pages/HomePage";
+import LibraryPage from "./pages/LibraryPage";
+import PlaylistDetailPage from "./pages/PlaylistDetailPage";
+import SearchPage from "./pages/SearchPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+
+const App = () => (
+<ThemeProvider defaultTheme="system" storageKey="dora-music-ui-theme">
+  <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+          <Routes>
+
+
+            <Route path="/" element={<HomePage />} />
+            <Route path="/artist-detail" element={<ArtistDetailPage />} />
+            <Route path="/library" element={<LibraryPage />} />
+            <Route path="/playlist-detail" element={<PlaylistDetailPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
+
+
+          </Routes>
+      </BrowserRouter>
+      </TooltipProvider>
+  </QueryClientProvider>
+</ThemeProvider>
+);
+
+export default App;
